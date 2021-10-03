855a04fc6b11faa3dd2fb991561591cf42d6fd6f
hiromu.saito <hiromu.saito@bigtreetc.com>
2021-10-03 17:14:02 +0900

    add flutterfire

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bulletin_board_app/components/rounded_button.dart';
+import 'package:bulletin_board_app/constant.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -10,10 +12,66 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  late String email;
+  late String password;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('registration'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'BBS',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+                fontSize: 50.0,
+              ),
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'enter your email',
+              ),
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              obscureText: true,
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'enter your password',
+              ),
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Register',
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
